31d164d4cfc91abd841a2bb44132f664e18933f5
Usman-99 <usmanyousuf1137@gmail.com>
2024-10-16 00:50:05 +0500

    updating date

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,20 @@ import { setSearch } from "../redux/slices/SearchSlice";
 
 const Navbar = () => {
   const dispatch = useDispatch();
+  let now = new Date();
+  let options = {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    timeZone: "Asia/Karachi",
+  };
+  let pakistanTime = now.toLocaleDateString("en-GB", options);
+
   return (
     <nav className="flex flex-col lg:flex-row justify-between py-3 mx-6 mb-10">
       <div>
-        <h3 className="text-xl font-bold text-gray-600">
-          {new Date().toUTCString().slice(0, 16)}
-        </h3>
+        <h3 className="text-xl font-bold text-gray-600">{pakistanTime}</h3>
         <h1 className="text-2xl font-bold ">Flavoro Foods</h1>
       </div>
       <div>
